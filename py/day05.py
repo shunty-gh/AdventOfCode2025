e84dab5782865e52c2577da460d167a343729a4f
Steven Hunt <sph@shunty.co.uk>
2025-12-05 10:15:53 +0000

    day 5, py

--- py/day05.py
+++ py/day05.py
@@ -17,3 +17,44 @@ for line in lines:
                 break
 
 print("Part 1:", part1)
+
+# Part 2 - Load all the ranges then split them up into a set of distinct, non-overlapping ranges
+allranges = set()
+for line in lines:
+    if not '-' in line:
+        break
+    ll,rr = line.split('-')
+    allranges.add((int(ll), int(rr)))
+
+# allranges now acts as a queue of sub-ranges that need to be processed
+fresh = set()
+while len(allranges) > 0:
+    lo, hi = allranges.pop()
+    # Check for overlaps
+    ok = True
+    for flo,fhi in fresh:
+        # Exact match or completely contained -> ignore it completely
+        if lo >= flo and hi <= fhi:
+            ok = False
+            break
+        # All above or all below -> try against the next saved range
+        if lo > fhi or hi < flo:
+            continue
+
+        # Must be an intersection
+        # Split into new ranges and add them to the queue for further processing
+        # lower range
+        if lo < flo:
+            allranges.add((lo, flo-1))
+        # upper range
+        if hi > fhi:
+            allranges.add((fhi+1, hi))
+        ok = False
+        break
+
+    # no part of the current range appears in any existing saved 'fresh' range, therefore add it
+    if ok:
+        fresh.add((lo, hi))
+
+part2 = sum([hi-lo+1 for lo,hi in fresh])
+print("Part 2:", part2)
