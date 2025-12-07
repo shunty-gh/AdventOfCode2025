97f3131d38817a3109f78090ffbd9493ad0fa144
Steven Hunt <sph@shunty.co.uk>
2025-12-07 10:17:22 +0000

    day 7, py

--- py/day07.py
+++ py/day07.py
@@ -1,4 +1,3 @@
-import re
 
 with open(f'../input/day07-input', 'r') as file:
     lines = [line.strip() for line in file.readlines()]
@@ -25,3 +24,27 @@ for y,line in enumerate(lines[1:]):
     current.update(next)
 
 print("Part 1:", part1)
+
+# Recursion and memoisation. The definition of hell on earth.
+# But it sure is quick.
+cache = dict[tuple[int,int], int]()
+def get_timelines_from(manifold: list[str], x: int, y: int) -> int:
+    # Have we seen this already
+    if (x,y) in cache:
+        return cache[(x,y)]
+
+    # Is next row the exit row?
+    if y+1 == len(manifold)-1:
+        return 1
+
+    # Cursed recursion. Find count from this col, or the left and right
+    # cols if there is a splitter directly below us
+    if manifold[y+1][x] == '^':
+        result = get_timelines_from(manifold, x-1, y+1) + get_timelines_from(manifold, x+1, y+1)
+    else:
+        result = get_timelines_from(manifold, x, y+1)
+    # Cache and return
+    cache[(x,y)] = result
+    return result
+
+print("Part 2:", get_timelines_from(lines, start, 0))
